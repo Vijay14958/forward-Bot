04374acc557a8331f4fd1b3e193d0112a1bed52d
Neha <116162193+Vijay14958@users.noreply.github.com>
2023-08-27 09:20:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     API_ID = int(os.environ.get("API_ID", "19134188"))
     API_HASH = os.environ.get("API_HASH", "91587601a5d898e3341b7e4a9e1c2537")       
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "5815403978:AAGvYKSsW-LTWkPynq30f5ISTvPsD7bfnWo")
-    BOT_SESSION = os.environ.get("BOT_SESSION", "forwardvjbot")
+    BOT_SESSION = os.environ.get("BOT_SESSION", "")
     OWNER_ID = os.environ.get("OWNER_ID", "1802523258")                             
     DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://sushankm16:4i1WAfPYKWyqPIDD@cluster0.sngp9pz.mongodb.net/?retryWrites=true&w=majority")  
     DATABASE_NAME = os.environ.get("DATABASE_NAME", "Clus")
